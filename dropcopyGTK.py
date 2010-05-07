21ed6a1fd9ed4d9b6afe0f3c286d838947b23c0c
Junior <juniorbl@gmail.com>
2010-05-06 21:36:21 -0300

    dropbox folder selection

--- dropcopyGTK.py
+++ dropcopyGTK.py
@@ -17,11 +17,14 @@ class DropCopyGTK:
 	_main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 	_fixed_container = gtk.Fixed()
 	
-	_file_chooser = None
+	_gnucash_file_chooser = None
+	_dropbox_file_chooser = None
 	
 	def __init__(self):
 		self._build_window()
-		self._build_file_chooser()
+		self._build_gnucash_file_chooser()
+		self._build_dropbox_file_chooser()
+		self._build_separator()
 		self._build_save_button()
 		self._build_cancel_button()
 		self._main_window.add(self._fixed_container)
@@ -33,29 +36,45 @@ class DropCopyGTK:
 		self._main_window.set_border_width(10)
 		self._main_window.set_title('DropCopy')
 		self._main_window.connect('destroy', self._destroy)
+	
+	def _build_separator(self):
+		separator = gtk.HSeparator()
+		self._fixed_container.put(separator, 40, 100)
 
-	def _build_file_chooser(self):
-		self._file_chooser = gtk.FileChooserButton('Select the GnuCash file')
-		self._file_chooser.set_current_folder(os.getenv('HOME'))
-		self._file_chooser.set_width_chars(self._FILE_CHOOSER_WIDTH)
-		self._fixed_container.put(self._file_chooser, 5, 1)
+	def _build_gnucash_file_chooser(self):
+		gnucash_label = gtk.Label('GnuCash file:')
+		self._fixed_container.put(gnucash_label, 5, 10)
+		self._gnucash_file_chooser = gtk.FileChooserButton('Select the GnuCash file')
+		self._gnucash_file_chooser.set_current_folder(os.getenv('HOME'))
+		self._gnucash_file_chooser.set_width_chars(self._FILE_CHOOSER_WIDTH)
+		self._fixed_container.put(self._gnucash_file_chooser, 115, 1)
+
+	def _build_dropbox_file_chooser(self):
+		dropbox_label = gtk.Label('Dropbox folder:')
+		self._fixed_container.put(dropbox_label, 5, 50)
+		self._dropbox_file_chooser = gtk.FileChooserButton('Select the dropbox folder') 
+		self._dropbox_file_chooser.set_action(gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
+		self._dropbox_file_chooser.set_current_folder(os.getenv('HOME'))
+		self._dropbox_file_chooser.set_width_chars(self._FILE_CHOOSER_WIDTH)
+		self._fixed_container.put(self._dropbox_file_chooser, 115, 40)
 
 	def _build_save_button(self):
 		save_button = gtk.Button('Save')
 		save_button.set_size_request(self._BUTTON_WIDTH, self._BUTTON_HEIGHT)
 		save_button.connect('clicked', self._save_dirs)
-		self._fixed_container.put(save_button, 5, 50)
+		self._fixed_container.put(save_button, 50, 80)
 
 	def _build_cancel_button(self):
 		cancel_button = gtk.Button('Cancel')
 		cancel_button.set_size_request(self._BUTTON_WIDTH, self._BUTTON_HEIGHT)
 		cancel_button.connect('clicked', self._destroy)
-		self._fixed_container.put(cancel_button, 100, 50)
+		self._fixed_container.put(cancel_button, 150, 80)
 
 	def _save_dirs(self, widget, data=None):
-		gnu_cash_file = self._file_chooser.get_filename()
+		gnu_cash_file = self._gnucash_file_chooser.get_filename()
+		dropbox_folder = self._dropbox_file_chooser.get_filename()
 		if gnu_cash_file != None:
-			print 'saving ' + gnu_cash_file
+			print 'Saved! GnuCash file: ' + gnu_cash_file + ' and Dropbox folder: ' + dropbox_folder
 		else:
 			self._build_and_show_error_dialog()
 
