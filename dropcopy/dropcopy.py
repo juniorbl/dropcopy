2b3e105458e0a8c0ee6cc96b8a0b3455cb3f19f5
Junior <juniorbl@gmail.com>
2010-05-11 20:39:11 -0300

    better class organization

--- dropcopy/dropcopy.py
+++ dropcopy/dropcopy.py
@@ -1,8 +1,10 @@
 # Initial point. Loads preferences and run the application
 
-import dropcopy_core
 import dropcopy_config
+import dropcopy_gtk
 import pyinotify
+import shutil
+import gtk
 import os
 	
 # Preference names
@@ -15,8 +17,20 @@ dropbox_folder = config.get_preference(DROPBOX_FOLDER_PREFERENCE)
 gnucash_folder, gnucash_file = os.path.split(gnucash_full_path)
 watch_manager = pyinotify.WatchManager()
 
+class Dropcopy(pyinotify.ProcessEvent):
+
+	# It seems that GnuCash only saves its file, it doesn't update
+	def process_IN_CREATE(self, event):
+		if event.name == gnucash_file:
+			shutil.copy(event.pathname, dropbox_folder + event.name)
+			self._inform_success()
+
+	def _inform_success(self):
+		dgtk = dropcopy_gtk.DropcopyGTK()
+		dgtk.show_notification('GnuCash file was copied to your local dropbox folder')
+
 def main():
-	dropcopy = dropcopy_core.DropcopyCore(gnucash_file, dropbox_folder)
+	dropcopy = Dropcopy()
 	notifier = pyinotify.Notifier(watch_manager, dropcopy)
 	when_saved = pyinotify.IN_CREATE
 	watch_manager.add_watch(gnucash_folder, when_saved, rec=True)
